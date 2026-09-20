7ac90177f99c75d9ea59a06da9b5e3491dcd2bd5
WillianServello <rafaelwillian299@gmail.com>
2026-09-19 21:20:56 -0300

    feat: working at variables css

--- cafeservello-UI/Services/Common/Theme/ThemeService.cs
+++ cafeservello-UI/Services/Common/Theme/ThemeService.cs
@@ -2,13 +2,15 @@
 {
     public sealed class ThemeService
     {
-        public string ThemeColor { get; private set; } = "";
+        public string ThemeColor { get; private set; } = "theme-classic";
 
         public event Action? OnChange;
 
         public void ToggleTheme()
         {
-            ThemeColor = ThemeColor == "light" ? "" : "light";
+            ThemeColor = ThemeColor == "theme-classic"
+                ? "theme-light"
+                : "theme-classic";
 
             OnChange?.Invoke();
         }
